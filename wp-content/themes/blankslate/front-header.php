c54728fab00eb891bbce606d612e4e99f4bcb199
James Mocko <james@cuecount.com>
2025-07-03 16:08:45 -0400

    fixing background of wp-block-group

--- wp-content/themes/blankslate/front-header.php
+++ wp-content/themes/blankslate/front-header.php
@@ -1,9 +1,7 @@
 <header id="header" role="banner" class="container-fullWidth" style="background-image: url('<?php echo wp_upload_dir()['baseurl'] . "/2024/12/1730244952843.jpg"; ?>');background-size:cover;background:#d7d7d7;">
     <div class="container-content header-padding">
         <h1>
-            James Mocko is a <br>
-            Data Engineer and <br>
-            Sophisticated Visual Storyteller
+            James Mocko is a <br>Automation Engineer and <br>Digital Designer
         </h1>
     </div>
 </header>
