cca68ae1246ac639d1a674184bbd93d184b2d344
James Mocko <jamesemocko@gmail.com>
2025-12-30 00:31:39 +0000

    Production sync: update blankslate theme

--- wp-content/themes/blankslate/front-header.php
+++ wp-content/themes/blankslate/front-header.php
@@ -1,7 +1,7 @@
-<header id="header" role="banner" class="container-fullWidth" style="background-image: url('<?php echo wp_upload_dir()['baseurl'] . "/2024/12/1730244952843.jpg"; ?>');background-size:cover;background:#d7d7d7;">
+<header id="header" role="banner" class="container-fullWidth" style="background-image: url('<?php echo wp_upload_dir()['baseurl'] . "/2025/07/jmocko_profile_shot_alt2.jpg"; ?>');background-size:cover;">
     <div class="container-content header-padding">
         <h1>
-            James Mocko is a <br>Automation Engineer and <br>Digital Designer
+            James Mocko is a <br>analytics thinker applied to <br>promotional campaign design
         </h1>
     </div>
 </header>
